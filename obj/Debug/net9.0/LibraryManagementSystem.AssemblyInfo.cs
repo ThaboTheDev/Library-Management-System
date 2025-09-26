b77ea8e3820bb2082d774d4a47d5676f88c85dc5
thmlilojhb024 <thmlilojhb024@student.wethinkcode.co.za>
2025-09-27 00:00:55 +0200

    chore: added the basic file structure for the project

--- obj/Debug/net9.0/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d9c993e532f55f3d015579adabbf59048fcf69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64f887c1b08651acbf9a326181b3d844422801f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
